f717a612d6e8843d26925abd9f8ebb352d7729d5
Ray Gabatino <hed-rrgabatino@smu.edu.ph>
2025-07-21 09:15:47 +0800

    Update OtherPlatform.jsx

--- dashboard/src/pages/UserPannel/OtherPlatform.jsx
+++ dashboard/src/pages/UserPannel/OtherPlatform.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import ClientSidebar from "./ClientSidebar";
 import jollibeeImg from "../../assets/Jollibee.png";
-import itBytesImg from '../../assets/ITBYTES.png';
+import itBytesImg from '../../assets/ItBytes.png';
 import NationalBImg from "../../assets/NationalB.jpeg";
 import BlendedImg from "../../assets/Blended.png";
 import TaraLabaImg from "../../assets/taraLaba.png";
