a17c5759118ae5ee2c0900058feaf9827431cbc0
Haaanashi <167769134+Haaanashii@users.noreply.github.com>
2026-08-08 13:13:53 +0800

    Add health check endpoint to confirm API is running

--- server/index.js
+++ server/index.js
@@ -45,6 +45,10 @@ app.use(cors({
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
+app.get("/", (req, res) => {
+  res.json({ message: "Dental Clinic API is running" });
+});
+
 // Connect to MongoDB
 connectDB();
 
